5a4a63127664fa95a791faf421dfbe6bd2c5f981
unknown <pfwang2@iflytek.com>
2020-05-24 13:24:50 +0800

    pwa add

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,14 +1,43 @@
+const moment = require('moment');
 module.exports = {
+  base: '/my-blog/',
   title: '雨巷的博客',
   description: '雨巷的博客',
+  plugins: [
+    [
+      '@vuepress/last-updated', {
+        transformer: (timestamp, lang) => {
+          const moment = require('moment')
+          moment.locale('zh-cn')
+          return moment(timestamp).fromNow("LLLL")
+        }
+      }
+    ],
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "发现新内容可用",
+          buttonText: "Refresh"
+        }
+      }
+    ]
+  ],
   head: [
     ['link', { rel: 'icon', href: '/assets/img/favicon.ico' }],
     ['meta', { name: 'author', content: '雨巷的博客' }],
-    ['meta', { name: 'keywords', content: '雨巷的博客' }]
+    ['meta', { name: 'keywords', content: '雨巷的博客' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
   ],
   themeConfig: {
     logo: '/assets/img/logo.png',
-
     nav: [
       { text: '主页', link: '/' },
       { text: 'deeplearn-admin', link: '/deeplearn-admin/' },
